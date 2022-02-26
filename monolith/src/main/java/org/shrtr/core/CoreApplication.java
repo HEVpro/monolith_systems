2b9cb49c3d5f28368c7875c5e96b64a75cf45231
hector <hector.evalverde@gmail.com>
2022-02-26 18:52:52 +0100

    feat: kafka environment files

--- monolith/src/main/java/org/shrtr/core/CoreApplication.java
+++ monolith/src/main/java/org/shrtr/core/CoreApplication.java
@@ -1,14 +1,19 @@
 package org.shrtr.core;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-public class CoreApplication {
+public class CoreApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
--- monolith/src/main/java/org/shrtr/core/events/KafkaConfiguration.java
+++ monolith/src/main/java/org/shrtr/core/events/KafkaConfiguration.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,10 +13,15 @@ import java.util.Properties;
 @Configuration
 public class KafkaConfiguration {
 
+    @Value("${kafka-host}")
+    private String host;
+    @Value("${kafka-port}")
+    private String port;
+
     @Bean
     public Admin kafkaAdmin() {
         Properties props = new Properties();
-        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, host + ":" + port);
         props.put(AdminClientConfig.RETRIES_CONFIG, "3");
         props.put(AdminClientConfig.RETRY_BACKOFF_MS_CONFIG, "2500");
         Admin admin = Admin.create(props);
